e3bca2d4c983057e15641123d876155bf7f1ea43
Ivan Webber <iwebber@email.arizona.edu>
2019-11-23 00:27:41 -0700

    Implemented ROT13Reader

--- ROT13.go
+++ ROT13.go
@@ -1,1 +1,46 @@
+package main
 
+import (
+	"io"
+	"os"
+	"strings"
+	"unicode"
+)
+
+type ROT13Reader struct {
+	r io.Reader
+}
+
+/*	The implementation of this method makes the ROT13Reader into a Reader by
+	implicitly satisfying the Reader interface!
+*/
+func (reader ROT13Reader) Read(out []byte) (int, error) {
+	n, err := reader.r.Read(out)
+
+	if err != nil {
+		return n, err
+	}
+
+	for i := range out {
+		ch := rune(out[i])
+
+		if unicode.IsLetter(ch) {
+			ltr := out[i] - 13
+
+			// wrap alphabet
+			if unicode.IsUpper(ch) && ltr < 'A' {
+				ltr = 'Z' - ('A' - ltr) + 1
+			} else if ltr < 'a' {
+				ltr = 'z' - ('a' - ltr) + 1
+			}
+			out[i] = ltr
+		}
+	}
+	return n, err
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := ROT13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
