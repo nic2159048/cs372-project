35fd0fed27189772ccf292d085e58bffe245fc3d
nic2159048 <35509591+nic2159048@users.noreply.github.com>
2019-11-25 06:01:18 -0700

    Update main.go

--- main/main.go
+++ main/main.go
@@ -1,10 +1,8 @@
 /*
-Uses the rot13 package to encrypt and decrypt files.
-
 This program attempts to conform to Go's style guidelines.
 
-Language Project: https://github.com/ivanthewebber/csc372-program4
-Language Study: https://github.com/ivanthewebber/csc372-program4/blob/master/language_study.md
+Language Project: https://github.com/nic2159048/cs372-project
+Language Study: https://github.com/nic2159048/cs372-project/blob/master/language_study.md
 */
 package main
 
@@ -15,7 +13,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/ivanthewebber/csc372-program3/rot13"
+	"unicode"
 )
 
 // splits filename and rotation & posts updates for the user
@@ -63,7 +61,7 @@ func rotateFiles(inputFilename string, outputFilename string, rot int) {
 	}
 	defer fileOut.Close()
 
-	rotReader := rot13.NewReader(fileIn, rot)
+	rotReader := NewReader(fileIn, rot)
 
 	var buff []byte
 	buff = make([]byte, 32)
@@ -79,6 +77,52 @@ func rotateFiles(inputFilename string, outputFilename string, rot int) {
 		}
 	}
 }
+
+// Reader ciphers and deciphers text in rot13.
+type Reader struct {
+	reader io.Reader
+	rotn   int
+}
+
+// NewReader is initialized and returned
+func NewReader(r io.Reader, n int) Reader {
+	return Reader{r, n}
+}
+
+/*	Read decodes or encodes text read in from a wrapped Reader.
+ */
+func (r Reader) Read(out []byte) (int, error) {
+	n, err := r.reader.Read(out)
+
+	if err != nil {
+		return n, err
+	}
+
+	for i := range out[:n] {
+		if ch := rune(out[i]); unicode.IsLetter(ch) {
+			ltr := byte(int(out[i]) + (r.rotn % 26))
+
+			// wrap alphabet
+			if unicode.IsUpper(ch) {
+				if ltr > 'Z' {
+					ltr = 'A' - 1 + (ltr - 'Z')
+				} else if ltr < 'A' {
+					ltr = 'Z' - ('A' - ltr) + 1
+				}
+			} else {
+				if ltr > 'z' {
+					ltr = 'a' - 1 + (ltr - 'z')
+				} else if ltr < 'a' {
+					ltr = 'z' - ('a' - ltr) + 1
+				}
+			}
+			out[i] = ltr
+		}
+	}
+	return n, err
+}
+
+
 
 func main() {
 	var inFilename, outFilename string
@@ -115,3 +159,6 @@ func main() {
 	rotateFiles(inFilename, outFilename, rot)
 	return
 }
+
+
+
